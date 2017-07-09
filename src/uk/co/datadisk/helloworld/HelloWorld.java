35591f2be1310419df53d26d79ff4f7daa9059fc
Paul Valle <paul.valle@datadisk.co.uk>
2017-07-09 17:26:46 +0100

    Traditional HelloWorld

---
+++ src/uk/co/datadisk/helloworld/HelloWorld.java
@@ -0,0 +1,15 @@
+package uk.co.datadisk.helloworld;
+
+import com.godtsoft.diyjava.*;
+/**
+ * Created by vallep on 09/07/2017.
+ *
+ */
+public class HelloWorld extends DIYWindow {
+    public HelloWorld() {
+    }
+
+    public static void main(String[] args) {
+
+    }
+}
