8e9a111b47d848e1b04d261d756105d408a761b5
Paul Valle <paul.valle@datadisk.co.uk>
2017-07-16 17:21:05 +0100

    create a few games to learn java

--- src/uk/co/datadisk/cards/Hand.java
+++ src/uk/co/datadisk/cards/Hand.java
@@ -8,4 +8,75 @@ import java.util.ArrayList;
 public class Hand {
     ArrayList<Card> cards = new ArrayList<>();
 
+    public void add(Card card) {
+        int index  = 0;
+        boolean done = false;
+
+        while(!done && index < cards.size()) {
+            Card cardInHand = cards.get(index);
+            if ( card.isGreaterThan(cardInHand)) {
+                index++;
+            } else {
+                done = true;
+            }
+        }
+        cards.add(index, card);
+    }
+
+    public void remove(Card card) {
+        int index = 0;
+        boolean found = false;
+
+        while(!found && index < cards.size()) {
+            Card comparedCard = cards.get(index);
+            if(comparedCard.equals(card)) {
+                cards.remove(index);
+                found = true;
+            } else {
+                index++;
+            }
+        }
+    }
+
+    public boolean contains(Card card) {
+        int index = 0 ;
+        boolean contains = false;
+
+        while(!contains && index < cards.size()) {
+            Card comparedCard = cards.get(index);
+            if(comparedCard.equals(card)) {
+                contains = true;
+            } else {
+                index++;
+            }
+        }
+
+        return contains;
+    }
+
+    public Card cardAt(int index) {
+        return cards.get(index);
+    }
+
+    public int size() {
+        return cards.size();
+    }
+
+    public String toString() {
+        String string = "";
+
+        for (int i = 0; i < cards.size(); i++) {
+            Card card = cards.get(i);
+            if (i != 0 ) {
+                Card priorCard = cards.get(i - 1);
+                char priorSuit = priorCard.getSuit();
+                char currentSuit = card.getSuit();
+                if( priorSuit != currentSuit) {
+                    string += "  ";
+                }
+            }
+            string += card.toString() + " ";
+        }
+        return string;
+    }
 }
--- src/uk/co/datadisk/crazyeights/CrazyEights.java
+++ src/uk/co/datadisk/crazyeights/CrazyEights.java
@@ -2,9 +2,11 @@ package uk.co.datadisk.crazyeights;
 
 import uk.co.datadisk.cards.Card;
 import uk.co.datadisk.cards.Deck;
+import uk.co.datadisk.cards.Hand;
 import uk.co.datadisk.mywindow.MyWindow;
 
 import java.util.ArrayList;
+import java.util.Random;
 
 /**
  * Created by vallep on 16/07/2017.
@@ -12,19 +14,111 @@ import java.util.ArrayList;
  */
 public class CrazyEights extends MyWindow {
 
-    public CrazyEights() {
-        Deck deck = new Deck();
-        ArrayList<Card> discardPile = new ArrayList<>();
+    private Deck deck = new Deck();
+    private Hand myHand = new Hand();
+    private Hand computerHand = new Hand();
+    private Card discard;
+    private ArrayList<Card> discardPile = new ArrayList<>();
+    Random rand = new Random();
+    private char activeSuit = ' ';
 
-        Card card;
-        for (int i = 0; i < 10; i++) {
-            card = deck.deal();
-            print(card.toString());
-            discardPile.add(card);
+
+    public CrazyEights() {
+
+        for (int i = 0; i < 7; i++) {
+            Card card1 = deal();
+            myHand.add(card1);
+
+            Card card2 = deal();
+            computerHand.add(card2);
         }
-        print("Deck Size: " + deck.size());
-        deck.reuse(discardPile);
-        print("Deck Size: " + deck.size());
+
+        discard = deal();
+
+        if( discard.getRank() == '8') {
+            activeSuit = discard.getSuit();
+        } else {
+            activeSuit = ' ';
+        }
+
+        playMyCard();
+        showStatus();
+    }
+
+    private Card deal() {
+        if (deck.size() == 0 ){
+            deck.reuse(discardPile);
+            deck.shuffle();
+            discardPile.clear();
+            print();
+            print("The discardPile has been shuffled");
+        }
+
+        Card card = deck.deal();
+        return card;
+    }
+
+    private void showStatus() {
+        print();
+        print("Computer has " + computerHand.toString() + " cards");
+        print("My Hand has " + myHand.toString() + " cards");
+        print("Discard: " + discard.toString());
+        if(discard.getRank() == '8') {
+            print("Suit is " + activeSuit);
+        }
+    }
+
+    private void drawMyCard() {
+        Card drewCard = deal();
+        print();
+        print("You drew " + drewCard.toString());
+        myHand.add(drewCard);
+
+        if(isValidPlay(drewCard.toString())) {
+            print("You played " + drewCard);
+            discardMycard(drewCard);
+        }
+    }
+
+    private void discardMycard(Card myCard) {
+        myHand.remove(myCard);
+        discardPile.add(discard);
+        discard = myCard;
+    }
+
+    private void playMyCard() {
+        showStatus();
+        boolean validPlay = false;
+        while (!validPlay) {
+            String rankSuit = promptForString("What card do you want to play (or D for draw)? ");
+            rankSuit = rankSuit.toUpperCase();
+            if (rankSuit.equals("D")) {
+                drawMyCard();
+                validPlay = true;
+            } else if (isValidPlay(rankSuit)){
+                Card selectedCard = new Card(rankSuit);
+                discardMycard(selectedCard);
+                validPlay = true;
+            }
+        }
+    }
+
+    private boolean isValidPlay(String rankSuit) {
+        boolean validPlay = true;
+        Card card = new Card(rankSuit);
+
+        if(!card.isValid()) {
+            print(rankSuit + " is not valid");
+            validPlay = false;
+        } else if (!myHand.contains(card)) {
+            print(rankSuit + " is not in hand");
+            validPlay = false;
+        } else if ( card.getSuit() != discard.getSuit() && card.getSuit() != discard.getSuit()) {
+            print(rankSuit + " cannot be played on " + discard + ".");
+            validPlay = false;
+        }
+
+
     }
 
     public static void main(String[] args) {
